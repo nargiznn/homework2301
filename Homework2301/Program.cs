127aba31338bbc20a7bc1ba70c907e5ba8621b5b
Nergiz Nadirli <nergiznadirli@Nergiz-MacBook-Air.local>
2024-01-23 14:27:51 +0400

    task

---
+++ Homework2301/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace Homework2301
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student std1 = new Student();
+            do
+            {
+                Console.Write("Ad: ");
+                std1.Name = Console.ReadLine();
+            } while (string.IsNullOrWhiteSpace(std1.Name));
+            do
+            {
+                Console.Write("Soyad: ");
+                std1.Surname = Console.ReadLine();
+            } while (!Student.CheckFullname(std1.Name, std1.Surname));
+
+            do
+            {
+                Console.Write("GroupNo (Axxx): ");
+                std1.GroupNo = Console.ReadLine();
+            } while (!Student.CheckGroupNo(std1.GroupNo));
+            string AgeStr;
+            byte Age;
+            do
+            {
+                Console.Write("Yaş: ");
+                AgeStr = Console.ReadLine();
+
+            } while (!byte.TryParse(AgeStr, out Age));
+            std1.Age = Age;
+            Console.WriteLine(std1.Fullname);
+            Console.WriteLine($"Tələbə məlumatları:\nName: {std1.Name}\nSurname: {std1.Surname}\nAge: {std1.Age}\nGroupNo: {std1.GroupNo}");
+
+
+
+        }
+    }
+}
+
---
+++ Homework2301/Student.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Reflection.Metadata;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace Homework2301
+{
+	public class Student
+	{
+		public Student()
+		{
+		}
+        private string _name;
+        private string _surname;
+        private string _fullname;
+        private string _groupNo;
+        private byte _age;
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public string Surname
+        {
+            get => _surname;
+            set => _surname = value;
+        }
+
+        public byte Age
+        {
+            get => _age;
+            set => _age = value;
+        }
+
+        public string Fullname
+        {
+            get => _fullname;
+            set
+            {
+                if (CheckFullname(Name, Surname))
+                    _fullname = value;
+                _fullname = Name + " " + Surname;
+            }
+        }
+ 
+		public string GroupNo
+		{
+			get => _groupNo;
+			set
+			{
+                if (CheckGroupNo(value))
+                {
+                    _groupNo = value;
+                }
+            }
+
+		}
+
+       public static bool CheckGroupNo(string groupNo)
+        {
+           
+            if (string.IsNullOrWhiteSpace(groupNo) || groupNo.Length != 4) return false;
+            if (!char.IsUpper(groupNo[0]))
+                return false;
+            if (!char.IsLetter(groupNo[0])) return false;
+			 for (int i = 1; i < 4; i++)
+        {
+            if (!char.IsDigit(groupNo[i])) return false;
+        }
+            return true;
+		}
+
+        public static bool CheckFullname(string name,string surname)
+        {
+            if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(surname))
+                return false;
+
+            for (int i = 0; i < name.Length; i++)
+			{
+				if (!char.IsLetter(name[i])) return false;
+
+			}
+            for (int i = 0; i < surname.Length; i++)
+            {
+                if (!char.IsLetter(surname[i])) return false;
+
+            }
+          
+            return true;
+
+        }
+		
+    }
+}
+
